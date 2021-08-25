7aa14f1fe82e44adb0c83d0208de30dc512534f5
Lyzlova Tatiana <yatanita@gmail.com>
2021-08-25 12:09:36 +0300

    First commit

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,21 @@
+package ru.netology.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CashbackHackServiceTest {
+
+    @Test
+    void shouldRemainIfAmountBelow1000() {
+
+        CashbackHackService service = new CashbackHackService();
+        int amount = 900;
+
+        int actual = service.remain(amount);
+        int expected = 100;
+        assertEquals(expected, actual);
+    }
+
+
+}
