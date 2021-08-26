63e046afefea6fa72c4b13e8457b9d926d37364e
Lyzlova Tatiana <yatanita@gmail.com>
2021-08-26 13:31:34 +0300

    Tests added

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -9,7 +9,7 @@ import static org.testng.AssertJUnit.assertEquals;
 
 class CashbackHackServiceTest {
 
-    @Test
+    @org.testng.annotations.Test
 
     void shouldRemainIfAmountBelow1000() {
 
@@ -20,6 +20,20 @@ class CashbackHackServiceTest {
         int expected = 100;
         assertEquals(actual, expected);
     }
+
+    @org.testng.annotations.Test
+
+    void shouldRemainIfAmount1000() {
+
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+
+        int actual = service.remain(amount);
+        int expected = 1000;
+        assertEquals(actual, expected);
+    }
+
+    @org.testng.annotations.Test
     void shouldRemainIfAmountOver1000() {
 
         CashbackHackService service = new CashbackHackService();
@@ -27,6 +41,28 @@ class CashbackHackServiceTest {
 
         int actual = service.remain(amount);
         int expected = 900;
+        assertEquals(actual, expected);
+    }
+
+    @org.testng.annotations.Test
+    void shouldRemainIfAmountMinus() {
+
+        CashbackHackService service = new CashbackHackService();
+        int amount = -800;
+
+        int actual = service.remain(amount);
+        int expected = 1800;
+        assertEquals(actual, expected);
+    }
+
+    @org.testng.annotations.Test
+    void shouldRemainIfAmount0() {
+
+        CashbackHackService service = new CashbackHackService();
+        int amount = 0;
+
+        int actual = service.remain(amount);
+        int expected = 1000;
         assertEquals(actual, expected);
     }
 
