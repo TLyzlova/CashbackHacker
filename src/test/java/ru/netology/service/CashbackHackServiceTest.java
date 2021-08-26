985efc3d8fbd8102fb460e7963618fcc3b57697b
Lyzlova Tatiana <yatanita@gmail.com>
2021-08-26 13:16:37 +0300

    Annotation changed

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,12 +1,16 @@
 package ru.netology.service;
 
-import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.Test;
+//
+//import static org.junit.jupiter.api.Assertions.*;
+import org.testng.annotations.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.testng.AssertJUnit.assertEquals;
 
 class CashbackHackServiceTest {
 
     @Test
+
     void shouldRemainIfAmountBelow1000() {
 
         CashbackHackService service = new CashbackHackService();
@@ -14,7 +18,16 @@ class CashbackHackServiceTest {
 
         int actual = service.remain(amount);
         int expected = 100;
-        assertEquals(expected, actual);
+        assertEquals(actual, expected);
+    }
+    void shouldRemainIfAmountOver1000() {
+
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1100;
+
+        int actual = service.remain(amount);
+        int expected = 900;
+        assertEquals(actual, expected);
     }
 
 }
