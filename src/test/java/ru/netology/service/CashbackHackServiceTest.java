b307daeb6dd05d72f40dcfa4abd57ce03f9b95cb
Lyzlova Tatiana <yatanita@gmail.com>
2021-08-26 13:53:10 +0300

    Test failed

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -7,6 +7,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class CashbackHackServiceTest {
 
     @Test
+
     void shouldRemainIfAmountBelow1000() {
 
         CashbackHackService service = new CashbackHackService();
@@ -14,8 +15,55 @@ class CashbackHackServiceTest {
 
         int actual = service.remain(amount);
         int expected = 100;
-        assertEquals(expected, actual);
+        assertEquals(actual, expected);
     }
 
+    @Test
+
+    void shouldRemain0IfAmount1000() {
+
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+
+        int actual = service.remain(amount);
+        int expected = 0;
+        assertEquals(actual, expected);
+    }
+
+    @Test
+
+    void shouldRemainIfAmountOver1000() {
+
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1100;
+
+        int actual = service.remain(amount);
+        int expected = 900;
+        assertEquals(actual, expected);
+    }
+
+    @Test
+
+    void shouldRemainIfAmountMinus() {
+
+        CashbackHackService service = new CashbackHackService();
+        int amount = -800;
+
+        int actual = service.remain(amount);
+        int expected = 1800;
+        assertEquals(actual, expected);
+    }
+
+    @Test
+
+    void shouldRemainIfAmount0() {
+
+        CashbackHackService service = new CashbackHackService();
+        int amount = 0;
+
+        int actual = service.remain(amount);
+        int expected = 1000;
+        assertEquals(actual, expected);
+    }
 
 }
